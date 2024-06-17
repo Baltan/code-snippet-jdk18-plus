b4ef16b3861fc62dcb166a7e48ba62fa37345a0d
baltan <617640006@qq.com>
2024-06-17 17:42:33 +0800

    未命名类和实例main方法 Demo

---
+++ src/main/java/class_test/MainTest.java
@@ -0,0 +1,10 @@
+/**
+ * Description:
+ *
+ * @author baltan
+ * @date 2024/6/17 17:39
+ * @see <a href="https://javaguide.cn/java/new-features/java21.html#jep-443-%E6%9C%AA%E5%91%BD%E5%90%8D%E6%A8%A1%E5%BC%8F%E5%92%8C%E5%8F%98%E9%87%8F-%E9%A2%84%E8%A7%88"></a>
+ */
+void main() {
+    System.out.println("Hello, World!");
+}
